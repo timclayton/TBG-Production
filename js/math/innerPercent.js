3ae5d604f2f561adbcd97da371863cd08269aad1
matt.diehl <matt.diehl@berndtgroup.net>
2012-11-15 19:57:35 -0500

    added comments

--- js/math/innerPercent.js
+++ js/math/innerPercent.js
@@ -1,6 +1,7 @@
 // Calculates the percent through a segment of a larger segment
 // Returns 0 before start of segment and one after end of segment
-// a = global percentage value
+// a, b, and c can either be all percentages or all integers
+// a = global value
 // b = segment start
 // c = segment end
 
